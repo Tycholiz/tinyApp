2cb2683e5679fbcf24079feb3753a7fefdf38600
Kyle Tycholiz <tycholiz22@hotmail.com>
2018-08-07 23:29:40 +0000

    urls page works

--- express_server.js
+++ express_server.js
@@ -4,12 +4,16 @@ var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs") //This tells the Express app to use EJS as its templating engine
 
-var urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {                               //this is a route handler. It tells us what will be rendered when the browser loads the '/' path of the website we are on.
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/urls", (req, res) => {                               //this is a route handler. It tells us what will be rendered when the browser loads the '/' path of the website we are on.
   res.render('urls_index', {
         urlDatabase: urlDatabase
   });
@@ -20,23 +24,27 @@ app.get("/urls.json", (req, res) => {                     //this is a route hand
 });
 
 app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/hola", (req, res) => {
   let templateVars = {
-    greeting: 'Hola Mundo'
+    greeting: 'Hello World!'
   };
-  res.render("hello_world", templateVars); //arg1 = the file w/in views to be rendered. arg2 = the object, where the key is the variable that will be included in hello_world, and the value, which is what that variable will render to
+  res.render("hello_world", templateVars);
 });
 
 app.get("/urls", (req, res) => {
   let templateVars = {
-    urls: urlDatabase
+    urls: urlDatabases
   };
   res.render("urls_index", templateVars);
 });
 
+// app.get("/urls/:id", (req, res) => {
+//   let templateVars = {
+//     shortURL: req.params.id
+//   };
+//   res.render("urls_show", templateVars);
+// });
+
 app.listen(PORT, () => {                                  //this is a route handler
   console.log(`Example app listening on port ${PORT}!`);
 });
+
