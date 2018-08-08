0f4117a23435849a4b4f27cd92112713edfc908f
Kyle Tycholiz <tycholiz22@hotmail.com>
2018-08-08 01:28:38 +0000

    generate random url and add to object

--- express_server.js
+++ express_server.js
@@ -4,10 +4,25 @@ var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs") //This tells the Express app to use EJS as its templating engine
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));  //this lets us see in the terminal the object, with key = longURL and value = https://lighthouselabs.ca. Without this, it will just return undefined. console.log(req.body) lets us see this when put into the app.post(/urls) route handler. remember that we must npm install bodyparser
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 6; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  return text;
+}
+
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -28,6 +43,17 @@ app.get("/urls", (req, res) => {                               //this is a route
 
 app.get("/urls.json", (req, res) => {                     //this is a route handler
   res.json(urlDatabase);
+});
+
+app.get("/urls/new", (req, res) => {          //this route handler must go before the one directly below it. they are both a page right after /urls/, so order matters here. in these cases, the more specific one will go first (this one is more specific because :id will change depending on what is passed through)
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  urlDatabase[generateRandomString()] = req.body.longURL;
+  console.log(urlDatabase);
 });
 
 app.get("/urls/:id", (req, res) => {
